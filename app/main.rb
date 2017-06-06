3f1ccd1cfc8a719aebc4d5a1ae4c8825d2752f73
ishikawa-pro <ishikawa.pro@gmail.com>
2017-06-06 19:28:59 +0900

    [add]コマンドラインからオプションを取得する部分の追加

--- app/main.rb
+++ app/main.rb
@@ -1,12 +1,26 @@
+require 'optparse'
+
 def main(argv)
   # このコードは引数と標準出力を用いたサンプルコードです。
   # このコードは好きなように編集・削除してもらって構いません。
   # ---
   # This is a sample code to use arguments and outputs.
   # Edit and remove this code as you like.
+  
+    opt = OptionParser.new    
+    input_file = String.new
+    output_file = String.new
+    #オプションごとのパラメータを取得
+    opt.on('-i', '--input_file ITEM','input_file path') { |v| 
+        input_file =  v
+    }
 
-  argv.each_index do |i|
-    v = argv[i]
-    puts "argv[#{i}]: #{v}"
-  end
+    opt.on('-o', '--output_file ITEM','output_file path') { |v| 
+        output_file = v
+    }
+    opt.parse(argv)
+    
+    File.open(input_file, "r") do |f|
+        puts f.read
+    end
 end
